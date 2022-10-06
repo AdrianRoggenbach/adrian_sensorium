db56d0f1c8092eea8492c8b24f8956c0ca08fff4
adrianhoffmann <hoffmann@hifo.uzh.ch>
2022-10-06 09:12:05 +0200

    Add regularization to modulator

--- neuralpredictors/layers/modulators/modulator.py
+++ neuralpredictors/layers/modulators/modulator.py
@@ -12,6 +12,7 @@ class HistoryGainModulator(nn.Module):
                  per_neuron_gain_adjust=False,
                  behav_state=False,
                  nr_behav_state=10,
+                 behav_alpha=0,
                  ):
         super().__init__()
         # save parameter
@@ -19,6 +20,7 @@ class HistoryGainModulator(nn.Module):
         self.include_history = include_history
         self.per_neuron_gain_adjust = per_neuron_gain_adjust
         self.behav_state = behav_state
+        self.behav_alpha = behav_alpha
         
         if self.per_neuron_gain_adjust:
             # initialize all with 1
@@ -45,20 +47,22 @@ class HistoryGainModulator(nn.Module):
         # gain: (batch, 1)
         # state: (batch, nr_states)
         
+        
         if self.include_history:
             # compute effect of history
             hist = torch.einsum( 'bnh,nh->bn', history, self.history_weights )
             hist = hist + self.history_bias
-            x = x + hist    # add history and image response
-
-        # compute reduced-rank prediction from last available timestep
+            x = x + hist    # add history
+            
+        # add additional signal based on the behavioral state
         if self.behav_state:
             state_mod = self.state_encoder( state )
-            x = x + state_mod
+            x = x + nn.functional.elu( state_mod )
             
-        # non-linearity
+        # non-linearity for final output of stimulus segment
         x = nn.functional.elu(x) + 1
         
+        # modify stimulus response with gain
         if self.include_gain:
             # multiply response with a gain factor
             if self.per_neuron_gain_adjust:
@@ -66,12 +70,20 @@ class HistoryGainModulator(nn.Module):
                 x = (1 + self.gain_adjust * gain)  *   x  
             else:
                 x = (1 + gain) * x 
-            
+
         return x
         
     def initialize(self, **kwargs):
         print('Initialize called but not implemented')
     
-    def regularizer(self, data_key):
-        return 0  #self[data_key].regularizer() * self.gamma_shifter
-    
+
+    def regularizer(self):
+        if self.behav_state and (self.behav_alpha > 0):
+            # L1 regularization
+            abs_weight_sum = 0
+            for p in self.state_encoder.parameters():
+                abs_weight_sum += p.abs().sum()
+            return abs_weight_sum * self.behav_alpha
+        
+        else:
+            return 0  #self[data_key].regularizer() * self.gamma_shifter
