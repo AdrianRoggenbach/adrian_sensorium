0f602635d8a52853d22d954d503575d6f989cdbe
adrianhoffmann <hoffmann@hifo.uzh.ch>
2022-10-07 16:42:49 +0200

    Improve learned gain regressor

--- neuralpredictors/layers/modulators/modulator.py
+++ neuralpredictors/layers/modulators/modulator.py
@@ -41,12 +41,12 @@ class HistoryGainModulator(nn.Module):
                                             bias=True )
             
             
-    def forward(self, x, history, gain, state, sort_id):
+    def forward(self, x, history, gain, state, rank_id):
         # x: (batch, nr_neurons) Output of the encoding model which uses images+behavior
         # history: (batch, nr_neurons, nr_lags)
         # gain: (batch, 1)
         # state: (batch, nr_states)
-        # sort_id is ignored (only used in HistoryOwnGainModulator, see below)
+        # rank_id is ignored (only used in HistoryOwnGainModulator, see below)
         
         
         if self.include_history:
@@ -116,18 +116,24 @@ class HistoryOwnGainModulator(nn.Module):
         self.diff_reg = diff_reg
         
         if self.include_gain:
-            self.own_gain = nn.Parameter( torch.zeros(nr_trials) )
+            max_val = np.sqrt( 1/nr_trials )
+            weights = torch.rand( (nr_trials) ) * (2*max_val) - max_val
+            self.own_gain = nn.Parameter( weights )
             # kernel is half a gaussian, to keep causal smoothing
             window = signal.gaussian(201, std=gain_kernel_std)
             window[0:100] = 0
+            window = window / np.sum(window)   # normalize to area 1
             self.gain_kernel = torch.zeros( (1,1,201) )
             self.gain_kernel[0,0,:] = torch.Tensor(window)
-            # self.gain_kernel[0,0,:] = torch.tensor( [0.05, 0.1, 0.2, 0.3, 0.2, 0.1, 0.05] )
             
             
         if self.per_neuron_gain_adjust:
-            # initialize all with 1
-            self.gain_adjust = nn.Parameter( torch.ones(nr_neurons) )
+            # initialize all close to zero
+            max_val_g = np.sqrt( 1/nr_neurons )
+            weights_g = torch.rand( (nr_neurons) ) * (2*max_val_g) - max_val_g
+            self.gain_coupling = nn.Parameter( weights_g )
+            self.coupling_offset = nn.Parameter( torch.zeros( 1 ) )
+            # this value will be mapped to positive only values with elu+1
         
         # initialize like linear layer, uniform between +-sqrt(1/nr)
         # https://pytorch.org/docs/stable/generated/torch.nn.Linear.html
@@ -144,12 +150,12 @@ class HistoryOwnGainModulator(nn.Module):
                                             bias=True )
             
             
-    def forward(self, x, history, gain, state, sort_id, device='cuda'):
+    def forward(self, x, history, gain, state, rank_id, device='cuda'):
         # x: (batch, nr_neurons) Output of the encoding model which uses images+behavior
         # history: (batch, nr_neurons, nr_lags)
         # gain: (batch, 1)
         # state: (batch, nr_states)
-        # sort_id: (batch, 1)
+        # rank_id: (batch, 1)
         
         
         if self.include_history:
@@ -163,32 +169,37 @@ class HistoryOwnGainModulator(nn.Module):
             state_mod = self.state_encoder( state )
             x = x + nn.functional.elu( state_mod )
             
-        # non-linearity for final output of stimulus segment
+
+        # non-linearity for final output of stimulus segment, only positive values
         x = nn.functional.elu(x) + 1
         
         # modify stimulus response with gain
         if self.include_gain:
-            # transform sort_ids into one-hot encoding
+            # transform rank_ids into one-hot encoding
             nr_batch = x.shape[0]
             one_hot = torch.zeros( (nr_batch, 1, self.nr_trials)).to(device)
-            for i, s_id in enumerate( sort_id[:,0] ):
-                one_hot[i,0,int(s_id)] = 1
+            for i, r_id in enumerate( rank_id[:,0] ):
+                one_hot[i,0,int(r_id)] = 1
                 
             # smooth one_hot encoding along trial dimension
             # one_hot has shape (batch, 1, nr_trials), interpreted as 1 channel
             # kernel has shape (1, 1, length), one input and one output channel
             self.gain_kernel = self.gain_kernel.to(device)
             smooth = nn.functional.conv1d(one_hot, self.gain_kernel, padding='same')
-            
+
             # scalar product between smooth and saved gain vector (along trials)
             batch_gain = torch.einsum( 'bet,t->be', smooth, self.own_gain )
-            
+
             # transform onto positive values only (0 mapped to 1)
             batch_gain = nn.functional.elu( batch_gain ) + 1
             
             if self.per_neuron_gain_adjust:
-                #       (nr_neurons)       (batch,1)  (batch,nr_neurons)
-                x = (1 + self.gain_adjust * batch_gain)  *   x  
+                # transform coupling values to 0 to pos values           # offset for all neurons
+                coupling_value = nn.functional.elu( self.gain_coupling + self.coupling_offset ) + 1
+                #                               (nr_neurons)    (batch,1)    
+                adj_gain = nn.functional.elu( coupling_value * (batch_gain-1) ) + 1  # (batch,nr_neurons)
+                
+                x = adj_gain * x  
             else:
                 x = batch_gain * x 
 
@@ -199,12 +210,19 @@ class HistoryOwnGainModulator(nn.Module):
     
 
     def regularizer(self):
+       
+        regularization = 0
+
         if self.include_gain and (self.diff_reg > 0):
             # L2 regularization of difference of gains
+            smooth_gain = self.own_gain.diff().square().sum() * self.diff_reg
+            regularization += smooth_gain
 
-            return self.own_gain.diff().square().sum() * self.diff_reg
-        
-        else:
-            return 0 
+        if self.per_neuron_gain_adjust and (self.behav_alpha  > 0):
+            sparse_gain_coupling = self.gain_coupling.abs().sum() * self.behav_alpha 
+            regularization += sparse_gain_coupling
+
+        return regularization
+
         
         
